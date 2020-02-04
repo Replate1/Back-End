ec4f7fabcecbcb811565a7d73753f6efb749774c
bamfranceschi <anna.franceschi@gmail.com>
2020-02-04 11:46:00 -0600

    removed truncate() from user-types while attempting to fix production seed error

--- database/seeds/001-user-types.js
+++ database/seeds/001-user-types.js
@@ -1,12 +1,10 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex("user_types")
-    .truncate()
-    .then(function() {
-      // Inserts seed entries
-      return knex("user_types").insert([
-        { user_type: "business" },
-        { user_type: "volunteer" }
-      ]);
-    });
+  return knex("user_types").then(function() {
+    // Inserts seed entries
+    return knex("user_types").insert([
+      { user_type: "business" },
+      { user_type: "volunteer" }
+    ]);
+  });
 };
